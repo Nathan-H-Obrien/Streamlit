07e345d1fa4363db5b2568e79401826baa6dd36e
Nathan O'Brien <nathan.obrien@jacks.sdstate.edu>
2024-12-05 10:12:22 -0600

    first commit

---
+++ website.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title('Hello World!')
+
+st.write('This is a simple example of a Streamlit web app.')
+
+st.write('You can write text, display data, and even create interactive widgets!')
+
+st.write('You can also add images, videos!')
+
+st.write('You can also add links to your web app!')
+
+st.write('You can also add code snippets!')
+
+st.write('You can also add mathematical equations!')
+
+st.write('You can also add tables!')
+
+st.write('You can also add charts!')
+
+st.write('You can also add maps!')
+
+st.write('You can also add sliders!')
+
+st.write('You can also add buttons!')
+
+st.write('You can also add checkboxes!')
+
+st.write('You can also add radio buttons!')
+
+st.write('You can also add select boxes!')
