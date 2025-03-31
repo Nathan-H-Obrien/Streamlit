98a30c6ced4fc3edb646054eac00ed044c7f7240
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2025-03-30 20:49:22 -0500

    Update Stocks.py

--- python/Stocks.py
+++ python/Stocks.py
@@ -8,7 +8,7 @@ def stock_page():
     st.title("Stocks")
 
     # Set up the API key
-    api_key = os.getenv("POLYGON_API_KEY")
+    api_key = "ZgFptzp39IOpFdfDnnpJUarLO9rN9jQ2"
     if not api_key:
         st.error("API key not found. Please set the POLYGON_API_KEY environment variable.")
         return
